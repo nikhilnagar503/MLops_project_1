46d5035410fd85843a2c4837bb174523b14900bc
nikhil nagar <nagarn603@gmail.com>
2025-05-18 22:05:47 +0530

    first commit of this project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nikhil Nagar",
+    author_email="nagarn603@gmail.com",
+    packages=find_packages()
+)
